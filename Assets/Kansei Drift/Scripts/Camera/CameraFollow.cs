372525e9befc69ad33b7e3354d63790bea4f001e
YoshuGH <axeleonardo2000@gmail.com>
2021-12-01 19:12:10 -0600

    Just a little tweaks

--- Assets/Kansei Drift/Scripts/Camera/CameraFollow.cs
+++ Assets/Kansei Drift/Scripts/Camera/CameraFollow.cs
@@ -6,21 +6,40 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Vector3 offset;
     [SerializeField] private Transform target;
-    [SerializeField] private float smoothTime = 0.3F;
-    private Vector3 velocity = Vector3.zero;
+    [SerializeField] private double smoothTime = 0.3F;
     [SerializeField] private float maxTranslateSpeed;
     [SerializeField] private float rotationSpeed;
+    private Vector3 velocity = Vector3.zero;
+    private float fVelocity = 0;
+    private CarController carController;
+
+    private void Start()
+    {
+        carController = target.GetComponent<CarController>();
+    }
 
     private void FixedUpdate()
     {
         HandleTranslation();
         HandleRotation();
+
+        if (carController != null)
+        {
+            if(carController.KMPH >= 25)
+            {
+                smoothTime = Mathf.SmoothDamp((float)smoothTime, 0.01f, ref fVelocity, 1.5f);
+            }
+            else
+            {
+                smoothTime = carController.KMPH / 75;
+            }
+        }
     }
 
     private void HandleTranslation()
     {
         var targetPos = target.TransformPoint(offset);
-        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime, maxTranslateSpeed);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, (float)smoothTime, maxTranslateSpeed);
     }
 
     private void HandleRotation()
--- Assets/Kansei Drift/Scripts/Car/CarController.cs
+++ Assets/Kansei Drift/Scripts/Car/CarController.cs
@@ -13,9 +13,12 @@ public class CarController : MonoBehaviour
     private bool isBraking, isHandbraking, updateCenterOfMass = false;
     private float currentBrakeForce;
     private float currentSteerAngle;
+    private float velocity = 0;
+    private float wheelBase = 2.55f;
+    private float wheelTrack = 1.5f;
+    private float kmph;
 
     private Rigidbody rb = new Rigidbody();
-    private float velocity = 0;
 
     public Text motor;
     public Text brake;
@@ -33,7 +36,7 @@ public class CarController : MonoBehaviour
     private float frontAntirollStiffness = 5000.0f;
     [SerializeField, Tooltip("The Value is express in N/m")] 
     private float rearAntirollStiffness = 5000.0f;
-    [SerializeField, Tooltip("Is for relocate the center of mass relative to the original, This can only see on play")] 
+    [SerializeField, Tooltip("Is for relocate the center of mass relative to the original")] 
     private Vector3 centerOfMassOffset;
 
     [Header("Wheel Colliders")]
@@ -48,17 +51,40 @@ public class CarController : MonoBehaviour
     [SerializeField] private Transform rearLeftTransform;
     [SerializeField] private Transform rearRightTransform;
 
+    public float KMPH
+    {
+        get { return kmph; }
+    }
+
     private void Start()
     {
+        Vector3 pos; Quaternion quat;
+
+        //Get the rigidbody of the player y change de center of mass depending of the offset setted before
+        //Also allow the Gizmos of the center of mass to see in game
         rb = transform.GetComponent<Rigidbody>();
         rb.centerOfMass += centerOfMassOffset;
         updateCenterOfMass = true;
+
+        //Obtain the position of need wheels to calculate wheelbase and wheeltrack
+        Vector3 FRWheel, RRWheel, FLWheel;
+        frontRightCollider.GetWorldPose(out pos, out quat);
+        FRWheel = pos;
+        rearRightCollider.GetWorldPose(out pos, out quat);
+        RRWheel = pos;
+        frontLeftCollider.GetWorldPose(out pos, out quat);
+        FLWheel = pos;
+
+        //Calculate wheelbase y wheeltrack
+        wheelBase = Vector3.Distance(FRWheel, RRWheel);
+        wheelTrack = Vector3.Distance(FRWheel,FLWheel);
     }
 
     void OnDrawGizmos()
     {
         Rigidbody _rb = this.GetComponent<Rigidbody>();
         Vector3 centerOfMassPos;
+        Vector3 pos; Quaternion quat;
 
         if (!updateCenterOfMass)
         {
@@ -71,6 +97,22 @@ public class CarController : MonoBehaviour
 
         Gizmos.color = Color.yellow;
         Gizmos.DrawSphere(centerOfMassPos, 0.1f);
+
+        Vector3 FRWheel, RRWheel, FLWheel;
+        frontRightCollider.GetWorldPose(out pos, out quat);
+        FRWheel = pos;
+        rearRightCollider.GetWorldPose(out pos, out quat);
+        RRWheel = pos;
+        frontLeftCollider.GetWorldPose(out pos, out quat);
+        FLWheel = pos;
+
+        //Wheelbase
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(FRWheel, RRWheel);
+
+        //Wheel track
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(FRWheel, FLWheel);
     }
 
     private void FixedUpdate()
@@ -83,6 +125,9 @@ public class CarController : MonoBehaviour
         //Calculate the Antirollforce for the antiroll bars in both axles (Front and Rear)
         AntirollBarCalculation(frontLeftCollider, frontRightCollider, frontAntirollStiffness);
         AntirollBarCalculation(rearLeftCollider, rearRightCollider, rearAntirollStiffness);
+
+        //Calculate Speed in KMPH
+        kmph = rb.velocity.magnitude * 3.6f;
 
         motor.text = "RPM: " + rearLeftCollider.rpm;
         brake.text = "Brake Torque R: " + rearLeftCollider.brakeTorque;
@@ -98,17 +143,21 @@ public class CarController : MonoBehaviour
 
     private void HandleMotor()
     {
+        float motorForceEachWheel = 0;
         switch(driveSetup)
         {
             case 0:
+                motorForceEachWheel = motorForce / 2;
                 frontLeftCollider.motorTorque = verticalInput * motorForce;
                 frontRightCollider.motorTorque = verticalInput * motorForce;
                 break;
             case (DriveSetup)1:
+                motorForceEachWheel = motorForce / 2;
                 rearLeftCollider.motorTorque = ((verticalInput * motorForce) / 3.14f) * 3.73f;
                 rearRightCollider.motorTorque = ((verticalInput * motorForce) / 3.14f) * 3.73f;
                 break;
             case (DriveSetup)2:
+                motorForceEachWheel = motorForce / 4;
                 frontLeftCollider.motorTorque = verticalInput * motorForce;
                 frontRightCollider.motorTorque = verticalInput * motorForce;
                 rearLeftCollider.motorTorque = verticalInput * motorForce;
@@ -153,7 +202,7 @@ public class CarController : MonoBehaviour
             rearRightCollider.brakeTorque = currentBrakeForce;
 
             forwardFriction.stiffness = Mathf.SmoothDamp(
-                rearLeftCollider.forwardFriction.stiffness, 0.8f, ref velocity, Time.deltaTime * 12f);
+                rearLeftCollider.forwardFriction.stiffness, 0.75f, ref velocity, Time.deltaTime * 10f);
             sidewaysFriction.stiffness = Mathf.SmoothDamp(
                 rearLeftCollider.sidewaysFriction.stiffness, 0.45f, ref velocity, Time.deltaTime * 2.5f);
 
@@ -166,9 +215,20 @@ public class CarController : MonoBehaviour
 
     private void HandleSteering()
     {
-        currentSteerAngle = maxSteeringAngle * horizontalInput;
-        frontLeftCollider.steerAngle = currentSteerAngle;
-        frontRightCollider.steerAngle = currentSteerAngle;
+        //Ackerman Steering formula
+        //steer angle = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (wheelRadius + (wheelTrack / 2))) * InputDirection
+        if (horizontalInput > 0 || horizontalInput < 0)
+        {
+            currentSteerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (frontLeftCollider.radius + (wheelTrack / 2))) * horizontalInput;
+            frontLeftCollider.steerAngle = Mathf.Clamp(currentSteerAngle, -maxSteeringAngle, maxSteeringAngle);
+            currentSteerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (frontRightCollider.radius + (wheelTrack / 2))) * horizontalInput;
+            frontRightCollider.steerAngle = Mathf.Clamp(currentSteerAngle, -maxSteeringAngle, maxSteeringAngle);
+        }
+        else
+        {
+            frontLeftCollider.steerAngle = 0;
+            frontRightCollider.steerAngle = 0;
+        }
     }
 
     private void UpdateWheels()
